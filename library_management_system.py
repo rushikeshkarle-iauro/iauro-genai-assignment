2ba7abd75d523d23ad37671bde3aaa73cdab543f
rushikeshkarle-iauro <rushikesh.karle@iauro.com>
2026-01-05 22:32:18 +0530

    Rushikesh-Library Management

--- library_management_system.py
+++ library_management_system.py
@@ -1,12 +1,14 @@
 
-class Book:
-
+class Book:    
+    """This is Book class called When every single book is to be added"""
     def __init__(self,name,id,author):
+        """This is constructor that assigns value to current instance"""
         self.name=name
         self.id=id
         self.author=author
 
     def display(self):
+        """This function is used to display current book"""
         print("Name:",self.name)
         print("id:",self.id)
         print("author:",self.author)
@@ -15,9 +17,11 @@ class Book:
 class library_management:
     
     def __init__(self):
+        """This is constructor called when objects are created"""
         self.books=[]
 
     def add_book(self):
+        """This function is called to Add books in the List"""
         print("Add details of the books")
         name=input("Enter name of the book:")
         id=int(input("Enter id of the book:"))
@@ -26,6 +30,7 @@ class library_management:
         self.books.append(book)
         
     def view_books(self):
+        """This function is used to view all the books"""
         if not self.books:
             print("Empty")
             return
@@ -34,6 +39,7 @@ class library_management:
         return
 
     def remove_book(self):
+        """This function is used to remove books based on id"""
         id=int(input("Enter id of the book to update for:"))
         for b in self.books:
             if b.id==id:
@@ -43,13 +49,16 @@ class library_management:
         print("Book not found to remove")
 
     def find_book(self):
+         """This function is used to find and display book based on Id"""
          id=int(input("Enter id of the book to update for:"))
          for b in self.books:
             if id==b.id:
                 Book.display(b)
                 return 
          print("Book not found:")
+
     def update_book(self):
+        """This function is used to update books name and author"""
         id=int(input("Enter id of the book to update for:"))
         for b in self.books:
              if id==b.id:
@@ -60,16 +69,18 @@ class library_management:
         print("Book not found")
  
 def main():
+    """This is starting point of program execution"""
     try:
         obj=library_management()
+        """Here object is created"""     
         while True:
           print("""Library Management System
-             1. ADD BOOK
-             2. VIEW BOOKS
-             3. REMOVE BOOKS
-             4. FIND BOOKS BY ID
-             5. UPDATE BOOK
-             6. EXIT""")
+1. ADD BOOK
+2. VIEW BOOKS
+3. REMOVE BOOKS
+4. FIND BOOKS BY ID
+5. UPDATE BOOK
+6. EXIT""")
 
           choice=int(input("Enter the choice:"))
           if choice==1:
@@ -89,7 +100,9 @@ def main():
             print("Invalid input given")
     except ValueError:
           print("Please enter the correct input")
+
 if __name__=="__main__":
+    """This is to define main method should be called first"""
     main()
 
 
